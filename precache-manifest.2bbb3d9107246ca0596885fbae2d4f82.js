e9d8dc7a965f779765278afb65992169977cd215
jurkotroll <jurko.bossetroll@gmail.com>
2019-02-24 23:51:14 +0100

    first commit

---
+++ precache-manifest.2bbb3d9107246ca0596885fbae2d4f82.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4634d7fe471dabbb4998",
+    "url": "/static/js/main.4634d7fe.chunk.js"
+  },
+  {
+    "revision": "95a629014c39b51fbc68",
+    "url": "/static/js/1.95a62901.chunk.js"
+  },
+  {
+    "revision": "485c203d6e63ae4090fb5283fec34646",
+    "url": "/index.html"
+  }
+];
